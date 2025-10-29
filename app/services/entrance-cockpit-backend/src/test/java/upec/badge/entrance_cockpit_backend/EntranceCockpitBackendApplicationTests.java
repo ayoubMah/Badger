d98b5ee43ddc2abb5a7e9915121125e1e48977f7
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-10-29 20:22:12 +0100

    feat: added servicecs: spring services

---
+++ app/services/entrance-cockpit-backend/src/test/java/upec/badge/entrance_cockpit_backend/EntranceCockpitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package upec.badge.entrance_cockpit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntranceCockpitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
